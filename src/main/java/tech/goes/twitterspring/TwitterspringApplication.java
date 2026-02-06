63b3e62da4c3350e1f398c6e8b2c297664e96827
Eduardo <edu.goes10@gmail.com>
2026-02-06 15:07:02 -0300

    first commit: creating entities

---
+++ src/main/java/tech/goes/twitterspring/TwitterspringApplication.java
@@ -0,0 +1,13 @@
+package tech.goes.twitterspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterspringApplication.class, args);
+	}
+
+}
